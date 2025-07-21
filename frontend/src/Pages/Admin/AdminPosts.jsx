18c6282c56cd65d13e3996acc412058b08d45776
joonsol <71648638+joonsol@users.noreply.github.com>
2025-07-21 11:16:32 +0900

    Update AdminPosts.jsx

--- frontend/src/Pages/Admin/AdminPosts.jsx
+++ frontend/src/Pages/Admin/AdminPosts.jsx
@@ -1,6 +1,30 @@
-import React from 'react'
 import "./AdminPosts.scss"
+import { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 const AdminPosts = () => {
+   const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      try {
+        const response = await axios.post(
+          `${import.meta.env.VITE_API_URL}/api/auth/verify-token`,
+          {}, // POST이므로 body 필요. 빈 객체라도 전달
+          {
+            withCredentials: true, // ✅ 쿠키 전송 필수
+          }
+        );
+        console.log("토큰 유효 ✅", response.data);
+      } catch (error) {
+        console.error("❌ 토큰 인증 실패", error);
+        navigate("/admin/login"); // 실패 시 로그인 페이지로
+      }
+    };
+
+    checkToken();
+  }, []);
+
   return (
     <div>AdminPosts</div>
   )
