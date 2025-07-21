0193a5a85349cebf55c265fba0ccecfb2927aa46
joonsol <71648638+joonsol@users.noreply.github.com>
2025-07-21 10:26:02 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,49 +1,49 @@
 require("dotenv").config()
-const express =require("express")
+const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
-const cookieParser=require("cookie-parser")
-const app =express()
-const PORT=3000;
+const cookieParser = require("cookie-parser")
+const app = express()
+const PORT = 3000;
 
 
 const userRoutes = require("./routes/user")
-const noticeRoutes=require("./routes/notice")
-const workRoutes=require("./routes/work")
-const uploadRoutes=require("./routes/upload")
-const contactRoutes=require("./routes/contact")
+const noticeRoutes = require("./routes/notice")
+const workRoutes = require("./routes/work")
+const uploadRoutes = require("./routes/upload")
+const contactRoutes = require("./routes/contact")
 
 
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    // "https://lix-co-project.vercel.app"  // 여기가 꼭 있어야 함!
+    "https://decode-lab.vercel.app/"
   ],
   credentials: true
 }))
 
 app.use(express.json())
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }))
 app.use(cookieParser())
 
-app.use("/api/auth",userRoutes)
-app.use("/api/contact",contactRoutes)
-app.use("/api/notice",noticeRoutes)
-app.use("/api/work",workRoutes)
-app.use("/api/upload",uploadRoutes)
+app.use("/api/auth", userRoutes)
+app.use("/api/contact", contactRoutes)
+app.use("/api/notice", noticeRoutes)
+app.use("/api/work", workRoutes)
+app.use("/api/upload", uploadRoutes)
 
-app.get("/",(req, res)=>{
-    res.send("hello world")
+app.get("/", (req, res) => {
+  res.send("hello world")
 })
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("mongodb와 연결 성공")
-}).catch((error)=>console.log("실패",error))
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("mongodb와 연결 성공")
+}).catch((error) => console.log("실패", error))
 
 console.log("✅ 현재 MONGO_URI 값:", process.env.MONGO_URI);
 
-app.listen(PORT,()=>{
-    console.log('Server is running')
+app.listen(PORT, () => {
+  console.log('Server is running')
 })
